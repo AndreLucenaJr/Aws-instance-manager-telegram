2f9e151ec2ce1fe21187e96911512952c4ac1120
AndreLucenaJr <andrelucenajr123@gmail.com>
2025-12-11 16:45:09 -0300

    (chore): remove check_table def and change main.py

--- main.py
+++ main.py
@@ -3,15 +3,13 @@ import asyncio
 from telegram.ext import Application
 from config import TELEGRAM_BOT_TOKEN
 from bot.bot_handler import setup_handlers
-from database.postgres import check_and_fix_columns
 from telegram import Update
 def main():
-    check_and_fix_columns()  
     application = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
     
     setup_handlers(application)
-
-    print("Bot iniciado e pronto para uso!")
+    print("=" * 40)
+    print("Bot Initialized")
     print("=" * 40)
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
